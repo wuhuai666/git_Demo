3a5992c248b5d1b567f7ea4c15a2a20d3591452a
wharxl <68179689+wuhuai666@users.noreply.github.com>
2021-08-26 17:47:28 +0800

    Update GitTest.java
    gitee测试

--- src/main/java/com/atguigu/gitTest/GitTest.java
+++ src/main/java/com/atguigu/gitTest/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("你觉得你又行了?");
         System.out.println("push test");
            System.out.println("pull test");
+        System.out.println("github项目更新了");
     }
 }
