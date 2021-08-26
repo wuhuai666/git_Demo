6b8604550a272d568d6e2cc5f71bc17e01670e28
wuhuai666 <3331670554@qq.com>
2021-08-26 12:21:08 +0800

    second IDEA commit

---
+++ src/main/java/com/atguigu/gitTest/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.gitTest;
+
+/**
+ * @author 吴淮
+ * @program git_Demo
+ * @description gitTestDemo
+ * @create 2021-08-26 12:20
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("HELLO GIt");
+    }
+}
