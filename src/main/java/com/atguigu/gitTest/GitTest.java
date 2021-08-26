db5c8c66d5c1cb1462d687314b6559df3850872e
wuhuai666 <3331670554@qq.com>
2021-08-26 12:39:08 +0800

    master IDEA commit

--- src/main/java/com/atguigu/gitTest/GitTest.java
+++ src/main/java/com/atguigu/gitTest/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("HELLO GIt");
         System.out.println("HELLO GIt");
         System.out.println("你的不太行");
+        System.out.println("你觉得你又行了?");
     }
 }
