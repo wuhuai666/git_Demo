56dfd7eef68253126260ab08fc9cc6cb4d90a725
wuhuai666 <3331670554@qq.com>
2021-08-26 12:27:36 +0800

    third IDEA commit

--- src/main/java/com/atguigu/gitTest/GitTest.java
+++ src/main/java/com/atguigu/gitTest/GitTest.java
@@ -9,5 +9,6 @@ package com.atguigu.gitTest;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("HELLO GIt");
+        System.out.println("HELLO GIt");
     }
 }
